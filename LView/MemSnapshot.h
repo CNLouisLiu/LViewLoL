609d6492cee0b6572c971746adb8cb0c5e8feb16
laur <laur.h_97@yahoo.ro>
2021-01-08 17:19:10 +0200

    Fixed identation and removed garbage files

--- LView/MemSnapshot.h
+++ LView/MemSnapshot.h
@@ -19,11 +19,11 @@ struct MemSnapshot {
 	std::vector<std::shared_ptr<GameObject>>      others;
 
 	/* A map between the network id of the object and the object itself */
-	std::map<int, std::shared_ptr<GameObject>>  objectMap;
-	std::map<short, int>                        indexToNetId;
+	std::map<int, std::shared_ptr<GameObject>>    objectMap;
+	std::map<short, int>                          indexToNetId;
 
 	/* Used to clear objectMap for objects that are no longer in game */
-	std::set<int>                               updatedThisFrame;
+	std::set<int>                                 updatedThisFrame;
 
 	/* The champion of the player running the app */
 	std::shared_ptr<GameObject>                   player = nullptr;
@@ -35,16 +35,16 @@ struct MemSnapshot {
 	std::unique_ptr<GameRenderer>                 renderer = std::unique_ptr<GameRenderer>(new GameRenderer());
 
 	/* How many seconds have elapsed since the game started */
-	float                                gameTime = 0.f;
+	float                                         gameTime = 0.f;
 
 	/* Stuff about the map the players are currently on */
-	std::shared_ptr<MapObject>           map;
-
-	/* Minimap related stuff */
-	Vector2                              minimapPos;
-	Vector2                              minimapSize;
-	
-	/* Memory reading benchmarks */
-	std::unique_ptr<ReadBenchmark>       benchmark = std::unique_ptr<ReadBenchmark>(new ReadBenchmark());
+	std::shared_ptr<MapObject>                    map;
+									             
+	/* Minimap related stuff */		             
+	Vector2                                       minimapPos;
+	Vector2                                       minimapSize;
+									             
+	/* Memory reading benchmarks */	             
+	std::unique_ptr<ReadBenchmark>                benchmark = std::unique_ptr<ReadBenchmark>(new ReadBenchmark());
 
 };
