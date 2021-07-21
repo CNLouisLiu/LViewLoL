ebbd99d77044766e439cd2cc0378c91dec01f950
orkido <orkido@mail.com>
2021-07-21 23:54:14 +0200

    Update offsets v11.15.3875736

--- LView/Offsets.cpp
+++ LView/Offsets.cpp
@@ -2,14 +2,14 @@
 
 Offsets::Offsets() {};
 
-int Offsets::GameTime = 0x2FA233C;
+int Offsets::GameTime = 0x02FB855C;
 
 int Offsets::ObjIndex = 0x20;
 int Offsets::ObjTeam = 0x4C;
 int Offsets::ObjNetworkID = 0xCC;
 int Offsets::ObjPos = 0x1d8;
 int Offsets::ObjVisibility = 0x270;
-int Offsets::ObjSpawnCount = 0x284;
+int Offsets::ObjSpawnCount = 0x218;
 int Offsets::ObjSrcIndex = 0x290;
 int Offsets::ObjMana = 0x298;
 int Offsets::ObjHealth = 0xD98;
@@ -24,7 +24,7 @@ int Offsets::ObjName = 0x2BBC;
 int Offsets::ObjLvl = 0x3344;
 int Offsets::ObjExpiry = 0x298;
 int Offsets::ObjCrit = 0x12C8;
-int Offsets::ObjCritMulti = 0x12B8;
+int Offsets::ObjCritMulti = 0x12B4;
 int Offsets::ObjAbilityPower = 0x122C;
 int Offsets::ObjAtkSpeedMulti = 0x12A0;
 int Offsets::ObjItemList = 0x3430;
@@ -33,9 +33,9 @@ int Offsets::ItemListItem = 0xC;
 int Offsets::ItemInfo = 0x20;
 int Offsets::ItemInfoId = 0x68;
 
-int Offsets::ViewProjMatrices = 0x2FCD840;
-int Offsets::Renderer = 0x2FD06AC;
-int Offsets::RendererWidth = 0x0C;
+int Offsets::ViewProjMatrices = 0x02FE46E0;
+int Offsets::Renderer = 0x02FE7544;
+int Offsets::RendererWidth = 0xC;
 int Offsets::RendererHeight = 0x10;
 
 int Offsets::SpellSlotLevel = 0x20;
@@ -46,9 +46,10 @@ int Offsets::SpellInfoSpellData = 0x44;
 int Offsets::SpellDataSpellName = 0x6C;
 int Offsets::SpellDataMissileName = 0x6C;
 
-int Offsets::ObjectManager = 0x170DC20;
-int Offsets::LocalPlayer = 0x2FAA664;
-int Offsets::UnderMouseObject = 0x2F9E9D8;
+int Offsets::ObjectManager = 0x017239D0;
+int Offsets::LocalPlayer = 0x02FC0128;
+int Offsets::UnderMouseObject = 0x02FB51E8;
+
 int Offsets::ObjectMapCount = 0x2C;
 int Offsets::ObjectMapRoot = 0x28;
 int Offsets::ObjectMapNodeNetId = 0x10;
@@ -60,7 +61,7 @@ int Offsets::MissileDestIdx = 0x310;
 int Offsets::MissileStartPos = 0x2D0;
 int Offsets::MissileEndPos = 0x2DC;
 
-int Offsets::MinimapObject = 0x2FAA0AC;
+int Offsets::MinimapObject = 0x02FC0890;
 
 int Offsets::MinimapObjectHud = 0x14;
 int Offsets::MinimapHudPos = 0x60;
